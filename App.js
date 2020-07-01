4613643c7b9d42108cc5770ad251aca68c8bb3ad
Ayush Rout <routayush1@gmail.com>
2020-07-01 17:53:16 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
-import React from "react";
+import { LineChart, Grid } from "react-native-svg-charts";
+import React, { useState } from "react";
+import * as shape from "d3-shape";
+
 import Covidscreen from "./screens/Covidscreen";
 
 export default function App() {
