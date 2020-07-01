70b5a92be36a3f921a2420656976c009e04ec242
Ayush Rout <routayush1@gmail.com>
2020-07-01 18:09:16 +0530

    hello

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { getDefaultConfig } = require("metro-config");
 
 module.exports = (async () => {
@@ -15,21 +14,3 @@ module.exports = (async () => {
     },
   };
 })();
-=======
-const { getDefaultConfig } = require("metro-config");
-
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig();
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve("react-native-svg-transformer"),
-    },
-    resolver: {
-      assetExts: assetExts.filter((ext) => ext !== "svg"),
-      sourceExts: [...sourceExts, "svg"],
-    },
-  };
-})();
->>>>>>> 5b65431300c5dbb1edf670d0c25c44de33a1d454
