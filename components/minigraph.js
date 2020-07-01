4771fa54fd49803848941d067de8bace3f8ed70a
Ayush Rout <routayush1@gmail.com>
2020-07-01 18:05:14 +0530

    enter

--- components/minigraph.js
+++ components/minigraph.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { LineChart, Grid } from "react-native-svg-charts";
 import React, { useState } from "react";
 import * as shape from "d3-shape";
@@ -20,26 +19,3 @@ export default function Minigraph({ type, color }) {
     ></LineChart>
   );
 }
-=======
-import { LineChart, Grid } from "react-native-svg-charts";
-import React, { useState } from "react";
-import * as shape from "d3-shape";
-import { useApi } from "../hooks/useApi";
-
-export default function Minigraph({ type, color }) {
-  const { dailylist } = useApi();
-  const every_nth = (arr, nth) => arr.filter((e, i) => i % nth === nth - 1);
-
-  return (
-    <LineChart
-      style={{ height: 70 }}
-      data={every_nth(dailylist[type], 3)}
-      svg={{ stroke: color }}
-      animate={true}
-      animationDuration={2000}
-      curve={shape.curveNatural}
-      contentInset={{ top: 20, bottom: 20 }}
-    ></LineChart>
-  );
-}
->>>>>>> 5b65431300c5dbb1edf670d0c25c44de33a1d454
