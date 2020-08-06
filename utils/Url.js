1e7c771047a208d2e708d597f93a0d8f028dba30
Ayush Rout <routayush1@gmail.com>
2020-08-06 19:11:37 +0530

    Add files via upload

---
+++ utils/Url.js
@@ -0,0 +1,10 @@
+module.exports = {
+  COVID_DATA: "http://healthx-api.herokuapp.com/getcoviddata",
+  HOSPITAL_DATA: "https://healthx-api.herokuapp.com/hospitaldata",
+  BEDS_DATA: "https://healthx-api.herokuapp.com/getbeds",
+  LABS_DATA: "https://healthx-api.herokuapp.com/gettestlabs",
+  REQUESTS: "https://healthx-api.herokuapp.com/getallreq",
+  LOGIN: "https://healthx-api.herokuapp.com/login",
+  POST: "https://healthx-api.herokuapp.com/newrequest",
+  DELETE: "https://healthx-api.herokuapp.com/delreq",
+};
